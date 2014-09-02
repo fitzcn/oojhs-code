7cd5a038e66c0450b22d6d689a7492062b64ca1b
Colin Fitzpatrick <fitzcn@u.northwestern.edu>
2014-09-01 23:30:04 -0500

    added three exercises for printing
    printing practice with string variable declaration, printing from
    arrays, and printing through loops.

---
+++ 20140902/printingFromArray.py
@@ -0,0 +1,27 @@
+"""
+T. Swift's "Shake it Off" is the current number one song on iTunes
+The first part of the chorus is as follows: 
+	Players gonna play, play, play, play, play,
+	Heartbreakers gonna break, break, break, break, break
+	I'm just gonna shake, shake, shake, shake, shake
+	Shake it off, shake it Off
+
+Part 1, concatenate string variables from the array list of words in the chorus from above
+(for example, chorusWords[0] will equal "gonna")
+
+Part 2, write print statements to print the lines of the song
+
+Part 3, clean up any code for better printing 
+"""
+
+
+chorusWords = ["gonna", "players", "play", "heartbreakers", "break", "I'm", "just", "gonna", "shake", "it", "off", ]
+#Part 1
+
+line1 = 
+line2 = 
+line3 = 
+line4 = 
+
+#Part 2
+
---
+++ 20140902/simplePrint.py
@@ -0,0 +1,9 @@
+#add values to these variables
+name = 
+age = 
+grade = 
+team = 
+favClass = 
+
+#add print statements below to print out the variables declared above
+
