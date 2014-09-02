53919d6d80a8622d09d29bfe040dfe4b04ced708
Colin Fitzpatrick <fitzcn@u.northwestern.edu>
2014-09-02 09:07:08 -0500

    updating for raw input on simple print

--- 20140902/simplePrint.py
+++ 20140902/simplePrint.py
@@ -1,9 +1,9 @@
-#add values to these variables
-name = 
-age = 
-grade = 
-team = 
-favClass = 
+#run this code in the interpretor and follow the prompts
+name = raw_input("Please enter your name: \n") 
+age = raw_input("Please enter your age: \n") 
+grade = raw_input("Please enter your grade: \n") 
+team = raw_input("Please enter your team: \n") 
+favClass = raw_input("Please enter your favorite class: \n") 
 
 #add print statements below to print out the variables declared above
 
