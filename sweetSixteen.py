1ca3e458a81ba7b2baaea3641f22bc69d527cd00
Colin Fitzpatrick <fitzcn@u.northwestern.edu>
2016-03-29 11:15:29 -0500

    done coding

---
+++ sweetSixteen.py
@@ -0,0 +1,38 @@
+sweetSixteen = ["Michigan St", "Kentucky", "Notre Dame", "Utah", "NC State", "Wichita St.", "Wisconsin", "Xavier", "Arizona", "West Virginia", "Louisville", "Oklahoma", "North Carolina", "Duke", "UCLA", "Gonzaga"]
+
+#print "Kentucky beats West Virigina" 
+
+#print "Notre Dame beats Wichita St" 
+
+#print "Wisconsin beats North Carolina" 
+
+#print "Arizona beats Xavier" 
+
+#print "Louisville beats NC State" 
+
+#print "Michigan St beats Oklahoma" 
+
+#print "Duke beats Utah" 
+
+#print "Gonzaga beats UCLA"
+
+#remove West Virginia
+
+#remove Wichita State
+
+#remove North Carolina 
+
+#remove Xavier
+
+#remove NC State
+
+#remove Oklahoma
+
+#remove Utah
+
+#remove UCLA
+
+print("The Elite 8 are:")
+sweetSixteen.sort()
+for team in sweetSixteen:
+	print(team)
